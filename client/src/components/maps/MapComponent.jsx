75f45c2e03a1fcb859352c5d2fd625f0b3721ddc
Nikash Das <nikash@Nikashs-MacBook-Pro-2.local>
2025-02-08 21:21:41 -0500

    updated maps

--- client/src/components/maps/MapComponent.jsx
+++ client/src/components/maps/MapComponent.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
 
+const GOOGLE_MAPS_API_KEY = "AIzaSyBoOak0ZV4Cln-yGCg0Z461MiGwemO6JqE";
+
 function MapComponent({children}) {
     return (
         <div>
             <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
                 <Map
                 style={{width: '100vw', height: '100vh'}}
-                defaultCenter={{lat: 40.7128, lng: 74.0060}}
-                defaultZoom={3}
+                defaultCenter={{lat: 40.808452, lng: -73.960}}
+                defaultZoom={12}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
                 >
