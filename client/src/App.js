2903d7ff0329a89f9737cb581c1fbf509d36058b
Aaron Liu <aaronliunJ@gmail.com>
2025-02-08 20:09:52 -0500

    Registration Form Commits

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Pages/home';
 import MapView from './Pages/map_view';
-
+import Register from './Pages/register'
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register/>}/>
         <Route path="/map_view" element={<MapView />} />
-  
+
       </Routes>
     </Router>
   );
