7127c8f3b993fc93499a27941a8fb4912c48ca9f
Nikash Das <nikash@Nikashs-MacBook-Pro-2.local>
2025-02-08 23:00:25 -0500

    updated maps again

--- client/src/components/maps/MapMarker.jsx
+++ client/src/components/maps/MapMarker.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import {AdvancedMarker, APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
 import { useState } from 'react';
 import { Modal, Box, Typography, Button, Popover} from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 const style = {
     position: 'absolute',
@@ -17,8 +19,36 @@ const style = {
     pb: 3,
   };
 
+  function CircularProgressWithLabel(props) {
+    return (
+      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+        <CircularProgress variant="determinate" {...props} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography
+            variant="caption"
+            component="div"
+            sx={{ color: 'text.secondary' }}
+          >{`${Math.round(props.value)}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
+  
+
 function MapMarkerComponent(props) {
     const [modal, setModal] = useState(false);
+    const [progress, setProgress] = React.useState(10);
     const [showHospital, setShowHospital] = useState(false);
     const id = showHospital ? 'simple-popover' : undefined;
 
@@ -27,6 +57,7 @@ function MapMarkerComponent(props) {
     return (
         <React.Fragment>
             <Marker position={{lat: props.details.lat, lng: props.details.lon}}
+                
                 onClick={() => {setModal(true)}}>
             
             </Marker>
@@ -36,10 +67,13 @@ function MapMarkerComponent(props) {
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
             >
-            <Box sx={{ ...style, width: "60vw", height: "80vh", display: "flex", flexDirection: "column", alignItems: "left"}}>
+
+            <Box sx={{ ...style, width: "60vw", height: "80vh", display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
                 {props.details.name}
                 </Typography>
+                <CircularProgressWithLabel value={34} />
+
                 <Button sx={{backgroundColor:"black",padding: "10px", display: "flex", flexDirection: "column", alignItems: "center"}} aria-describedby={id} variant="contained" onClick={() => {setShowHospital(!showHospital)}}>
                     Overall Rating: {props.details.satisfaction_summary_stats["Overall hospital rating"]}
                     {showHospital && Object.keys(props.details.satisfaction_summary_stats).map((stat, index) => (
